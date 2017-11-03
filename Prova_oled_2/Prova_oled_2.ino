dff86db7d0de6a8d2162194fc29cad78dff3254a
Enrico Menegatti\Casa <enricomenegatti13@gmail.com>
2017-11-03 23:05:51 +0100

    CREATA BUONA PROVA BLUETHOOT. APP PER TELEFONO "BLUETHOOT TERMINAL HC-05"

--- Prova_oled_2/Prova_oled_2.ino
+++ Prova_oled_2/Prova_oled_2.ino
@@ -19,23 +19,6 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16 
-static unsigned char PROGMEM const logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
 
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
@@ -45,6 +28,14 @@ const int focus = 8;
 const int shoot = 9;
 
 int serial = 0;
+
+void clean()
+{
+
+  display.clearDisplay();   // clears the screen and buffer
+  display.display();
+  
+}
 
 void setup()   
 {  
@@ -68,9 +59,8 @@ void setup()
   
   display.display(); // show splashscreen
   delay(2000);
-  display.clearDisplay();   // clears the screen and buffer
   
-  display.display();
+  clean();
 
 }
 
@@ -97,8 +87,7 @@ void loop()
   
         delay(1000);
         
-        display.clearDisplay();   // clears the screen and buffer
-        display.display();
+        //clean();
         
         digitalWrite(focus, LOW);
 
@@ -117,8 +106,7 @@ void loop()
         
         delay(200);
         
-        display.clearDisplay();   // clears the screen and buffer
-        display.display();
+        //clean();
         
         digitalWrite(shoot, LOW);
 
--- prova_bluethoot/prova_bluethoot.ino
+++ prova_bluethoot/prova_bluethoot.ino
@@ -1,28 +1,21 @@
-#include <SoftwareSerial.h>// import the serial library
-
-SoftwareSerial Genotronex(12, 11); // RX, TX
-int ledpin=13; // led on D13 will show blink on / off
-int BluetoothData; // the data given from Computer
 
 void setup() {
-  // put your setup code here, to run once:
-  Genotronex.begin(9600);
-  Genotronex.println("Bluetooth On please press 1 or 0 blink LED ..");
-  pinMode(ledpin,OUTPUT);
-}
-
+Serial.begin(19200);
+ pinMode(8, OUTPUT); // put your setup code here, to run once:
+ }
+ 
 void loop() {
   // put your main code here, to run repeatedly:
-   if (Genotronex.available()){
-BluetoothData=Genotronex.read();
-   if(BluetoothData=='1'){   // if number 1 pressed ....
-   digitalWrite(ledpin,1);
-   Genotronex.println("LED  On D13 ON ! ");
+ if(Serial.available()>0)
+   {     
+      char data= Serial.read(); // reading the data received from the bluetooth module
+      switch(data)
+      {
+        case 'a': digitalWrite(8, HIGH);break; // when a is pressed on the app on your smart phone
+        case 'd': digitalWrite(8, LOW);break; // when d is pressed on the app on your smart phone
+        default : break;
+      }
+      Serial.println(data);
    }
-  if (BluetoothData=='0'){// if number 0 pressed ....
-  digitalWrite(ledpin,0);
-   Genotronex.println("LED  On D13 Off ! ");
-  }
-}
-delay(100);// prepare for next data ...
+   delay(50);
 }
