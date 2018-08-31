da14d0ec650a1b0444886e6a182947216570a26c
CASA\Enrico <enricomenegatti13@gmail.com>
2018-08-31 19:12:49 +0200

    creato scatto remoto semplice e avanzato con javascript.

---
+++ HTML_Camera/main.js
@@ -0,0 +1,68 @@
+var rainbowEnable = false;
+var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);
+
+connection.onopen = function () {
+    connection.send('Connect ' + new Date());
+};
+connection.onerror = function (error) {
+    console.log('WebSocket Error ', error);
+};
+connection.onmessage = function (e) {
+    console.log('Server: ', e.data);
+};
+connection.onclose = function(){
+    console.log('WebSocket connection closed');
+};
+
+function Func_mod() {
+    var Mod_S = document.getElementById("Semplice");
+    var Mod_A = document.getElementById("Avanzata");
+    var Btn = document.getElementById("Btn_modalita");
+
+    if(Mod_A.style.display === "inline")
+    {
+      Mod_A.style.display = "none";
+      Btn.innerHTML = "Semplice";
+
+    }
+    else
+    {
+      Mod_A.style.display = "inline";
+      Btn.innerHTML = "Avanzata";
+    }
+}
+
+function sendRGB() {
+    var r = document.getElementById('r').value**2/1023;
+    var g = document.getElementById('g').value**2/1023;
+    var b = document.getElementById('b').value**2/1023;
+
+    var rgb = r << 20 | g << 10 | b;
+    var rgbstr = '#'+ rgb.toString(16);
+    console.log('RGB: ' + rgbstr);
+    connection.send(rgbstr);
+}
+
+function rainbowEffect(){
+    rainbowEnable = ! rainbowEnable;
+    if(rainbowEnable){
+        connection.send("R");
+        document.getElementById('rainbow').style.backgroundColor = '#00878F';
+        document.getElementById('r').className = 'disabled';
+        document.getElementById('g').className = 'disabled';
+        document.getElementById('b').className = 'disabled';
+        document.getElementById('r').disabled = true;
+        document.getElementById('g').disabled = true;
+        document.getElementById('b').disabled = true;
+    } else {
+        connection.send("N");
+        document.getElementById('rainbow').style.backgroundColor = '#999';
+        document.getElementById('r').className = 'enabled';
+        document.getElementById('g').className = 'enabled';
+        document.getElementById('b').className = 'enabled';
+        document.getElementById('r').disabled = false;
+        document.getElementById('g').disabled = false;
+        document.getElementById('b').disabled = false;
+        sendRGB();
+    }
+}
